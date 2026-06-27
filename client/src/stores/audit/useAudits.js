601f1c8b0613278eef810ceffd98326e2497528d
Chuckie <chuckie12602@gmail.com>
2026-06-28 07:43:49 +0800

    feat: add Zustand store for audit logs management with CRUD actions

---
+++ client/src/stores/audit/useAudits.js
@@ -0,0 +1,64 @@
+import { create } from 'zustand';
+import { auditLogsService } from '../../services';
+import { action } from '../utilities';
+
+export const useAuditLogs = create((set, get) => ({
+    // --- States ---
+    auditLogs: [],
+    activeAuditLog: null,
+    isLoading: false,
+    error: null,
+
+    // --- Reads ---
+    getAll: action(set, async () => {
+        const auditLogs = await auditLogsService.getAll();
+
+        set({ auditLogs: auditLogs });
+        return auditLogs;
+    }),
+    getById: action(set, async (id) => {
+        const auditLog = await auditLogsService.getById(id);
+        return auditLog;
+    }),
+    getByActorId: action(set, async (actorId) => {
+        const auditLogs = await auditLogsService.getByActorId(actorId);
+
+        set({ auditLogs: auditLogs });
+        return auditLogs;
+    }),
+    getByEntityType: action(set, async (entityType) => {
+        const auditLogs = await auditLogsService.getByEntityType(entityType);
+
+        set({ auditLogs: auditLogs });
+        return auditLogs;
+    }),
+    getByEntityId: action(set, async (entityId) => {
+        const auditLogs = await auditLogsService.getByEntityId(entityId);
+
+        set({ auditLogs: auditLogs });
+        return auditLogs;
+    }),
+
+    // --- Locals ---
+    selectActiveAuditLog: action(set, async (id) => {
+        const auditLog = get().auditLogs.find((al) => al.id === id);
+        const activeAuditLog = auditLog ?? (await auditLogsService.getById(id));
+
+        set({ activeAuditLog: activeAuditLog });
+        return activeAuditLog;
+    }),
+    deselectActiveAuditLog: () => {
+        set({ activeAuditLog: null });
+    },
+
+    // --- Actions ---
+    create: action(set, async (data) => {
+        const createdAuditLog = await auditLogsService.create(data);
+
+        const auditLogs = get().auditLogs;
+        const newAuditLogs = [...auditLogs, createdAuditLog];
+
+        set({ auditLogs: newAuditLogs });
+        return createdAuditLog;
+    }),
+}));

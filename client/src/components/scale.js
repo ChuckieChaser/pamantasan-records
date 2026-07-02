6b48d6e8e45c948da53e6c68ee029dcc900a8c8a
Chuckie <chuckie12602@gmail.com>
2026-07-02 10:30:22 +0800

    feat: add layout and scale components with size exports

---
+++ client/src/components/scale.js
@@ -0,0 +1,51 @@
+// --- Global Sizes ---
+export const UI_SIZES = {
+    small: 'h-8 px-3 text-sm',
+    medium: 'h-10 px-4 text-sm',
+    large: 'h-12 px-6 text-base',
+};
+
+export const ICON_SIZES = {
+    small: 'size-4',
+    medium: 'size-5',
+    large: 'size-6',
+};
+
+// --- Button Specific Classes ---
+export const ICON_BUTTON_SIZES = {
+    small: 'size-8',
+    medium: 'size-10',
+    large: 'size-12',
+};
+
+export const TRACK_SWITCH_SIZES = {
+    small: 'w-8 h-5 p-0.5',
+    medium: 'w-11 h-6 p-1',
+    large: 'w-14 h-8 p-1',
+};
+
+export const THUMB_SWITCH_SIZES = {
+    small: 'size-4',
+    medium: 'size-4',
+    large: 'size-6',
+};
+
+export const THUMB_SWITCH_TRANSLATE = {
+    small: 'translate-x-3',
+    medium: 'translate-x-5',
+    large: 'translate-x-6',
+};
+
+// --- Textfield Specific Classes ---
+export const TEXTAREA_SIZES = {
+    small: 'py-2 px-3 text-sm',
+    medium: 'py-3 px-4 text-sm',
+    large: 'py-4 px-6 text-base',
+};
+
+// --- Badge Specific Classes ---
+export const BADGE_SIZES = {
+    small: 'h-5 px-2 text-xs',
+    medium: 'h-6 px-3 text-xs',
+    large: 'h-8 px-4 text-sm',
+};
---
+++ client/src/components/ui/index.js
@@ -0,0 +1,3 @@
+export * from './Buttons';
+export * from './Textfields';
+export * from './Backdrops';
